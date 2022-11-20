2c4ba8cf154de5d81d8a6a19c966c8edf2148fc9
urvashiparashar <urvashiparashar2001@gmail.com>
2022-11-20 19:05:55 +0530

    updated

--- src/GlobalStyle.jsx
+++ src/GlobalStyle.jsx
@@ -23,4 +23,34 @@ h1
     font-size: 6rem;
     font-weight: 900;
 }
-`
+h2
+{
+    color: ${({theme})=> theme.colors.heading};
+    font-size: 4.4rem;
+    font-weight: 300;
+    white-space: normal;
+    text-align: center;
+}
+h3
+{
+    font-size: 1.8rem;
+    font-weight : 400;
+}
+p
+{
+    color: ${({theme})=> theme.colors.text};
+    opacity: .7;
+    font-size: 1.65 rem;
+    line-height: 1.5;
+    margin-top: 1rem;
+    font-weight: 400;
+}
+a
+{
+    text-decoration: none;
+}
+li
+{
+    list-style:none;
+}
+`;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,27 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import Navbar from './Navbar';
-
+import styled from 'styled-components';
 const Header = () => {
   return (
-    <header id="header" class="fixed-top d-flex align-items-center ">
+    <Mainheader>
     <NavLink to="/">
     <img src="./images/logo.png" alt="logo"/>
     </NavLink>
     <Navbar/>
     
-  </header>
+    </Mainheader>
   );
   
 };
+
+const Mainheader=styled.header`
+padding: 0 4.8rem;
+height: 10rem;
+background-color: ${({theme})=>theme.colors.bg};
+// display:flex;
+// justify-content: space-between;
+// align-items: center;
+`;
 
 export default Header;
